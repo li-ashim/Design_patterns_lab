08a3b4e8ebb9d760c40715a3095060177304f4cf
Mikhail Shchukin <misha2002mg@mail.ru>
2022-04-17 20:56:46 +0300

    Make DrinkBuilder class, get rid of DrinkBuilderDirector, TeaBuilder

--- src/TeaCoffeeMenu/Main.java
+++ src/TeaCoffeeMenu/Main.java
@@ -1,15 +1,11 @@
 package TeaCoffeeMenu;
 
 import TeaCoffeeMenu.Customer.Customer;
-import TeaCoffeeMenu.Drinks.DrinkBuilderDirector;
-import TeaCoffeeMenu.Drinks.TeaBuilder;
 
 public class Main {
     public static void main(String[] args) {
 
         Menu menu = Menu.getInstance();
-        DrinkBuilderDirector dbd = DrinkBuilderDirector.getInstance();
-        dbd.addDrinkBuilder("чай", new TeaBuilder());
 
         Customer c = new Customer();
         c.addDrinkToOrder();
